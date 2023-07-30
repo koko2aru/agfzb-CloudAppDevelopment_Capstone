72f6eee3bd98948113c8b7d48f0001e8515cdb77
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-07-30 16:05:54 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,7 +15,8 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def view(request):
+    return render("index.html")
 
 # Create an `about` view to render a static about page
 # def about(request):
