7b20b2599e542d311b27b25d4c2bafcc17d21a2b
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-07-31 13:29:39 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,17 +13,15 @@ import json
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
-
 # Create an `about` view to render a static about page
 def about(request):
     context = {}
-    return render(request, 'djangoapp/about.html' context)
+    return render(request, 'djangoapp/about.html', context)
 
 # Create a `contact` view to return a static contact page
 def contact(request):
     context = {}
-    return render(request, 'djangoapp/contact.html' context)
+    return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 def login_request(request):
