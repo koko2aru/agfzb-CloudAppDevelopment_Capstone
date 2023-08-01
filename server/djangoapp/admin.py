5254da62225444bf2701431655d34566f87097a1
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-08-01 16:33:15 +0200

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,11 +5,11 @@ from .models import CarMake, CarModel
 # Register your models here.
 
 class CarModelInline(admin.StackedInline):
-    model = CarModrl
+    model = CarModel
     extra = 3
 
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ('name')
+    list_display = ('name', 'dealer_id')
     list_filter = ['type', 'year']
 
 class CarMakeAdmin(admin.ModelAdmin):
