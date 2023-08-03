6195c6ce82ad0356c3925e558ad95c0e68c035f1
Yukari <80179287+koko2aru@users.noreply.github.com>
2023-08-03 15:31:10 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -62,7 +62,7 @@ def get_dealer_by_id_from_cf(url, dealerId):
         
         for review in reviews:
             review_doc = review["doc"]
-            review_obj = DealerView(dealership = review_doc["dealership"], name = review_doc["name"], purchase = review_doc["purchase"], review = review, purchase_date = purchase_date, car_make = car_make, car_model = car_model, sentiment = sentiment, id = id)
+            review_obj = DealerView(dealership = review_doc["dealership"], name = review_doc["name"], purchase = review_doc["purchase"], review = review_doc["review"], purchase_date = review_doc["purchase_date"], car_make = review_doc["car_make"], car_model = review_doc["car_model"], sentiment = review_doc["sentiment"], id = review_doc["id"])
             results.append(review_obj)
     return results
 
